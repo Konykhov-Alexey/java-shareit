73f945becee09a00ea3571264dc54442e2ca5d36
Алексей <aaalexskortoshka@gmail.com>
2026-03-11 20:45:01 +0600

    create: schema.sql, entity

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,22 +1,36 @@
 package ru.practicum.shareit.item.model;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.Builder;
-import lombok.AllArgsConstructor;
+import lombok.*;
+import jakarta.persistence.*;
 import ru.practicum.shareit.request.ItemRequest;
 
 import ru.practicum.shareit.user.model.User;
 
-@Data
+@Entity
 @Builder
+@Data
+@Table(name = "items")
+@EqualsAndHashCode(of = "id")
 @AllArgsConstructor
 @NoArgsConstructor
 public class Item {
+
+    @Id
+    @Column(name = "item_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @Column(name = "description", nullable = false)
     private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id", nullable = false)
     private User owner;
+
+    @Column(name = "is_available", nullable = false)
     private Boolean available;
     private ItemRequest request;
 }
--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,15 +1,28 @@
 package ru.practicum.shareit.user.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-@Data
+
+@Entity
 @Builder
+@Table(name = "users")
+@Data
 @EqualsAndHashCode(of = "id")
 public class User {
 
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
+
 }
