2eca6bd41d305eb4b45db266cd88e42bdf4f5d52
Алексей <aaalexskortoshka@gmail.com>
2026-03-14 00:10:49 +0600

    fixs

--- src/main/java/ru/practicum/shareit/util/RequestHeaderConstants.java
+++ src/main/java/ru/practicum/shareit/util/RequestHeaderConstants.java
@@ -1,7 +1,5 @@
 package ru.practicum.shareit.util;
 
-import lombok.experimental.UtilityClass;
-
 public class RequestHeaderConstants {
 
     public static final String USER_ID_HEADER = "X-Sharer-User-Id";
